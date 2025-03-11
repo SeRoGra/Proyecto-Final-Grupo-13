a8454d0b286211770a03065c09050edc39a8c6b6
olssonbianca <olssonbiancanadine@gmail.com>
2025-03-10 23:08:53 -0300

    terraform files

---
+++ locals.tf
@@ -0,0 +1,22 @@
+locals {
+  capacity      = "mundose"
+  functionality = "pinfinal"
+
+  resource_tags = {
+    capacity      = local.capacity
+    functionality = local.functionality
+    terraform     = "true"
+    environment   = "dev"
+  }
+}
+
+locals {
+  vpc_id    = "vpc-0c7b99973da428a73"
+  subnet_id = "subnet-064998e47c2bed4f2"
+}
+
+locals {
+  keypair_name = "key-${local.capacity}-${local.functionality}"
+  security_group_name = "${local.capacity}-${local.functionality}"
+  ec2_instance_name   = "ec2-${local.capacity}-instance-${local.functionality}"
+}
